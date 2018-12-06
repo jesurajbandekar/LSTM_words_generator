eb708e8d0c7603ad9192992491b5096c3c811b00
Jesuraja Bandekar <jesurajbndkr.jb@gmail.com>
2018-12-07 00:23:49 +0530

    Add files via upload

---
+++ lstm_model.py
@@ -0,0 +1,18 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+
+
+#The model is trained for 20 epochs  
+
+model = Sequential()
+model.add(LSTM(256, input_shape=(maxlen,len(chars))))
+model.add(Dense(len(chars), activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+
+
+
+
